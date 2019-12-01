96558b3647e6cf19f4faf746b8eff00a58c56dc6
Thomas Salzinger <thomas@salzinger.me>
2019-12-01 11:24:00 +0100

    Setup Gradle 6.0.1 and Kotlin 1.3.61

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "me.salzinger"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+}
