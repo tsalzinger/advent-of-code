c8c5d37d29b341d62961e7b893d1de7889951f63
Thomas Salzinger <ths@tractive.com>
2019-12-04 08:34:42 +0100

    Day 4 - Strict password candidate count

--- src/main/kotlin/DayFour.kt
+++ src/main/kotlin/DayFour.kt
@@ -20,14 +20,48 @@ fun Int.meetsPasswordCriteria(): Boolean {
     return valid && hasDouble
 }
 
+fun Int.meetsPasswordCriteriaStrict(): Boolean {
+    val password = toString()
+    var maxNum: Int = Int.MIN_VALUE
+    var valid = password.length == 6
+    var hasDouble = false
+    var index = 0
+    var groupLength = 1
+    while (valid && index < password.length) {
+        val current = password[index].toInt()
+        if (current == maxNum) {
+            groupLength++
+        } else {
+            hasDouble = hasDouble || groupLength == 2
+            groupLength = 1
+        }
+
+        valid = valid && current >= maxNum
+        maxNum = max(maxNum, current)
+        index++
+    }
+
+
+    return valid && (hasDouble || groupLength == 2)
+}
+
 fun main() {
     solvePuzzle7()
+    solvePuzzle8()
 }
 
 private fun solvePuzzle7() {
     7.solve {
         convertToRange()
             .filter(Int::meetsPasswordCriteria)
+            .count()
+            .toString()
+    }
+}
+private fun solvePuzzle8() {
+    8.solve {
+        convertToRange()
+            .filter(Int::meetsPasswordCriteriaStrict)
             .count()
             .toString()
     }
--- src/test/kotlin/DayFourTests.kt
+++ src/test/kotlin/DayFourTests.kt
@@ -25,4 +25,25 @@ class PasswordTests {
                 )
             }
         }
+    @TestFactory
+    fun `meets password criteria strict`() =
+        listOf(
+            122345 to true,
+            111123 to false,
+            135679 to false,
+            111111 to false,
+            223450 to false,
+            123789 to false,
+            112233 to true,
+            123444 to false,
+            111122 to true
+        ).map {
+            DynamicTest.dynamicTest("Password  ${it.first} matches strict rules is ${it.second}") {
+                Assertions.assertEquals(
+                    it.second,
+                    it.first
+                        .meetsPasswordCriteriaStrict()
+                )
+            }
+        }
 }
