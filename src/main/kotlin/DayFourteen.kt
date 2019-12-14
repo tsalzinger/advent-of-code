a96e1ee42ad1d32c90271c1cf618c726e2b32bb3
Thomas Salzinger <thomas@salzinger.me>
2019-12-14 13:05:01 +0100

    Day 14 - Maximum fuel production

--- src/main/kotlin/DayFourteen.kt
+++ src/main/kotlin/DayFourteen.kt
@@ -6,52 +6,91 @@ fun main() {
     27.solve {
         toChemicalReactionsMap()
             .run {
+                val chemicalStore = mutableMapOf<Chemical, Long>().withDefault { 0 }
+
                 getValue(Fuel)
                     .inputs
                     .map {
-                        getNecessaryOre(it)
+                        getNecessaryOre(it, chemicalStore)
                     }
 
                 chemicalStore[Ore]
             }
             .toString()
     }
+
+    28.solve {
+        toChemicalReactionsMap()
+            .run {
+                getValue(Fuel)
+                    .inputs
+                    .run {
+                        val chemicalStore = mutableMapOf<Chemical, Long>().withDefault { 0 }
+                        val availableOre = 1_000_000_000_000
+                        var fuelAmount = 0
+
+                        while (chemicalStore.getValue(Ore) < availableOre) {
+                            map {
+                                getNecessaryOre(it, chemicalStore)
+                            }
+
+                            if (chemicalStore.getValue(Ore) <= availableOre) {
+                                fuelAmount++
+                                println("$fuelAmount produced, consumed ${chemicalStore.getValue(Ore)} ore")
+                            } else {
+                                println("Stopping production, consumed ${chemicalStore.getValue(Ore)} ore")
+                            }
+                        }
+
+                        fuelAmount
+                    }
+            }
+            .toString()
+    }
 }
 
-val chemicalStore = mutableMapOf<Chemical, Int>().withDefault { 0 }
+typealias ChemicalStore = MutableMap<Chemical, Long>
 
 typealias Recipes = Map<Chemical, Reaction>
 
-fun Map<Chemical, Reaction>.getNecessaryOre(chemicalInputs: Set<ChemicalInput>, level: Int = 0) {
+fun Map<Chemical, Reaction>.getNecessaryOre(
+    chemicalInputs: Set<ChemicalInput>,
+    chemicalStore: ChemicalStore,
+    level: Int = 0
+) {
     chemicalInputs
         .forEach {
-            getNecessaryOre(it, level)
+            getNecessaryOre(it, chemicalStore, level)
         }
 }
 
-fun Map<Chemical, Reaction>.getNecessaryOre(chemicalInput: ChemicalInput, level: Int = 0) {
+fun Map<Chemical, Reaction>.getNecessaryOre(
+    chemicalInput: ChemicalInput,
+    chemicalStore: ChemicalStore,
+    level: Int = 0
+) {
     val requiredChemical = chemicalInput.chemical
     val requiredAmount = chemicalInput.amount
     val availableAmount = chemicalStore.getValue(requiredChemical)
     val prefix = List(level) { "|---" }.joinToString("")
 
     if (requiredChemical == Ore) {
-        println("$prefix$chemicalInput requires ${chemicalInput.amount} ORE")
+        //println("$prefix$chemicalInput requires ${chemicalInput.amount} ORE")
         chemicalStore[requiredChemical] = availableAmount + requiredAmount
-        println("Total required ORE: ${chemicalStore[requiredChemical]}")
+        //println("Total required ORE: ${chemicalStore[requiredChemical]}")
     } else {
         if (availableAmount >= requiredAmount) {
-            println("$prefix$chemicalInput still $availableAmount available in store, consuming $requiredAmount")
+            //println("$prefix$chemicalInput still $availableAmount available in store, consuming $requiredAmount")
             chemicalStore[requiredChemical] = availableAmount - requiredAmount
-            println("$prefix$chemicalInput has ${chemicalStore[requiredChemical]} remaining in store")
+            //println("$prefix$chemicalInput has ${chemicalStore[requiredChemical]} remaining in store")
         } else {
-            println("$prefix$chemicalInput only $availableAmount available in store")
+            //println("$prefix$chemicalInput only $availableAmount available in store")
             val necessaryAmount = requiredAmount - availableAmount
             val reaction = getValue(requiredChemical)
             val productionAmount = reaction.output.amount
-            val multiple = ceil(necessaryAmount.toFloat() / productionAmount.toFloat()).toInt()
+            val multiple = ceil(necessaryAmount.toFloat() / productionAmount.toFloat()).toLong()
 
-            println("$prefix$chemicalInput producing $multiple times $reaction to get $necessaryAmount")
+            //println("$prefix$chemicalInput producing $multiple times $reaction to get $necessaryAmount")
             chemicalStore[requiredChemical] = availableAmount + multiple * productionAmount - requiredAmount
 
             getNecessaryOre(
@@ -62,6 +101,7 @@ fun Map<Chemical, Reaction>.getNecessaryOre(chemicalInput: ChemicalInput, level:
                             amount = it.amount * multiple
                         )
                     }.toSet(),
+                chemicalStore,
                 level = level + 1
             )
         }
@@ -91,17 +131,17 @@ val Ore = Chemical("ORE")
 val Fuel = Chemical("FUEL")
 
 data class ChemicalOutput(
-    val amount: Int,
+    val amount: Long,
     val chemical: Chemical
 )
 
 fun String.toChemicalOutput(): ChemicalOutput {
     val (amount, chemicalName) = split(" ")
-    return ChemicalOutput(amount.trim().toInt(), Chemical(chemicalName.trim()))
+    return ChemicalOutput(amount.trim().toLong(), Chemical(chemicalName.trim()))
 }
 
 data class ChemicalInput(
-    val amount: Int,
+    val amount: Long,
     val chemical: Chemical
 )
 
@@ -112,7 +152,7 @@ fun String.toChemicalInputs(): Set<ChemicalInput> {
 
 fun String.toChemicalInput(): ChemicalInput {
     val (amount, chemicalName) = split(" ")
-    return ChemicalInput(amount.trim().toInt(), Chemical(chemicalName.trim()))
+    return ChemicalInput(amount.trim().toLong(), Chemical(chemicalName.trim()))
 }
 
 data class Reaction(
