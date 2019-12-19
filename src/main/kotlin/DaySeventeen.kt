d48a8cb93c1395460de9fe176d3b90c60394a9d3
Thomas Salzinger <ths@tractive.com>
2019-12-19 23:47:13 +0100

    Day 17 - Space dust collection

--- src/main/kotlin/DaySeventeen.kt
+++ src/main/kotlin/DaySeventeen.kt
@@ -1,5 +1,7 @@
 package me.salzinger
 
+import java.math.BigInteger
+
 val DRONE_VALUES = setOf('^', 'v', '<', '>')
 
 data class Pixel(
@@ -17,6 +19,10 @@ class CameraOutput(private val pixels: Map<Point, Pixel>) : Map<Point, Pixel> by
         }.map { (point, _) -> point }
     }
 
+    fun print() = pixels.print { value, _ ->
+        value?.value.toString()
+    }
+
     private fun Point.countAdjacentScaffolds(): Int {
         return listOf(
             up(1),
@@ -29,40 +35,92 @@ class CameraOutput(private val pixels: Map<Point, Pixel>) : Map<Point, Pixel> by
     }
 }
 
+fun Memory.getCameraOutput(): CameraOutput {
+    return IntcodeProgramInterpreter(
+        memory = this
+    ).evaluate()
+        .executionContext
+        .output
+        .getOutput()
+        .map(Number::toChar)
+        .run {
+            val scaffoldMap = mutableMapOf<Point, Pixel>()
+            var currentPosition = Point(0, 0)
+
+            forEach {
+                currentPosition = if (it == '\n') {
+                    currentPosition.down(1).copy(x = 0)
+                } else {
+                    scaffoldMap[currentPosition] = Pixel(
+                        it
+                    )
+                    currentPosition.right(1)
+                }
+            }
+
+            CameraOutput(scaffoldMap.toMap())
+        }
+}
+
+fun List<String>.toAsciiInput(): List<BigInteger> {
+    return "${joinToString("\n")}\n".toAsciiInput()
+}
+
+fun String.toAsciiInput(): List<BigInteger> {
+    return map { it.toInt().toBigInteger() }
+}
+
 fun main() {
     33.solve {
         first()
             .convertIntcodeInput()
+            .getCameraOutput()
+            .run {
+                val intersectionsCount = intersections
+                    .map { (x, y) -> x * -y }
+                    .sum()
+                "${print()}\n\n$intersectionsCount"
+            }
+    }
+
+    34.solve {
+        first()
+            .convertIntcodeInput()
             .run {
                 IntcodeProgramInterpreter(
-                    memory = this
+                    memory = this,
+                    overrides = mapOf(0 to BigInteger.TWO),
+                    inputs = ListInputProvider(
+                        listOf(
+                            "A,B,B,A,B,C,A,C,B,C",
+                            "L,4,L,6,L,8,L,12",
+                            "L,8,R,12,L,12",
+                            "R,12,L,6,L,6,L,8",
+                            "n"
+                        ).toAsciiInput()
+                    ),
+                    outputRecorder = object : ListOutputRecorder() {
+                        override fun addValue(value: BigInteger) {
+                            super.addValue(value)
+                            if (value <= 126.toBigInteger()) {
+                                print("${value.toInt().toChar()}")
+                            } else {
+                                print(value)
+                            }
+                        }
+                    }
                 )
             }
             .evaluate()
             .executionContext
             .output
             .getOutput()
-            .map(Number::toChar)
-            .run {
-                val scaffoldMap = mutableMapOf<Point, Pixel>()
-                var currentPosition = Point(0, 0)
-
-                forEach {
-                    currentPosition = if (it == '\n') {
-                        currentPosition.up(1).copy(x = 0)
-                    } else {
-                        scaffoldMap[currentPosition] = Pixel(
-                            it
-                        )
-                        currentPosition.right(1)
-                    }
-                }
-
-                CameraOutput(scaffoldMap.toMap())
-            }
-            .intersections
-            .map { (x, y) -> x * y }
-            .sum()
+            .last()
             .toString()
     }
 }
+// A,B,B,A,B,C,A,C,B,C
+//val m = "L,4,L,6,L,8,L,12,L,8,R,12,L,12,L,8,R,12,L,12,L,4,L,6,L,8,L,12,L,8,R,12,L,12,R,12,L,6,L,6,L,8,L,4,L,6,L,8,L,12,R,12,L,6,L,6,L,8,L,8,R,12,L,12,R,12,L,6,L,6,L,8"
+//val A = "L,4,L,6,L,8,L,12"
+//val B = "L,8,R,12,L,12"
+//val C = "R,12,L,6,L,6,L,8"
