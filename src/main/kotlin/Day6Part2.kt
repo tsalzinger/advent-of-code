0040fb40bc864d3bde0aaf0b36dd3b97b6c1869c
Thomas Salzinger <ths@tractive.com>
2021-12-06 19:55:04 +0100

    Day 6 - 2

--- src/main/kotlin/Day6Part2.kt
+++ src/main/kotlin/Day6Part2.kt
@@ -3,9 +3,9 @@ package me.salzinger
 import java.math.BigDecimal
 
 fun main() {
-    6.solve(1) {
+    6.solve(2) {
         flatMap { it.toIntList() }
-            .sumOf { BigDecimal.ONE + Lanternfish.getNumberOfOffspringAfterDays(it, 80) }
+            .sumOf { BigDecimal.ONE + Lanternfish.getNumberOfOffspringAfterDays(it, 256) }
             .toString()
     }
 }
--- src/main/kotlin/Lanternfish.kt
+++ src/main/kotlin/Lanternfish.kt
@@ -1,20 +1,24 @@
 package me.salzinger
 
+import java.math.BigDecimal
+
 object Lanternfish {
     private const val SPAWN_DURATION = 7
     private const val INITIAL_SPAWN_DELAY = 8
+
+    private val cache = mutableMapOf<Pair<Int, Int>, BigDecimal>()
 
     fun doesSpawnOffspring(spawnDelay: Int, day: Int): Boolean {
         return day > spawnDelay && (day - (spawnDelay + 1)) % SPAWN_DURATION == 0
     }
 
-    fun getNumberOfOffspringAfterDays(spawnDelay: Int, days: Int): Int {
-        return (1..days).sumOf { day ->
+    fun getNumberOfOffspringAfterDays(spawnDelay: Int, days: Int): BigDecimal {
+        return cache[spawnDelay to days] ?: (1..days).sumOf { day ->
             if (doesSpawnOffspring(spawnDelay, day)) {
-                1 + getNumberOfOffspringAfterDays(INITIAL_SPAWN_DELAY, days - day)
+                BigDecimal.ONE + getNumberOfOffspringAfterDays(INITIAL_SPAWN_DELAY, days - day)
             } else {
-                0
+                BigDecimal.ZERO
             }
-        }
+        }.also { cache[spawnDelay to days] = it }
     }
 }
