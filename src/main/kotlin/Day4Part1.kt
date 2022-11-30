b4629a88d4048253149ff49efe1a3313261d3dbc
Thomas Salzinger <thomas.salzinger@tractive.com>
2022-11-30 20:54:22 +0100

    Prepare for 2022

--- src/main/kotlin/Day4Part1.kt
+++
@@ -1,9 +0,0 @@
-package me.salzinger
-
-fun main() {
-    4.solve(1) {
-        val initialGame = BingoParser.parse(this)
-
-        BingoWinningPlayer(initialGame).getWinningScore().toString()
-    }
-}
--- src/main/kotlin/Day4Part2.kt
+++
@@ -1,9 +0,0 @@
-package me.salzinger
-
-fun main() {
-    4.solve(2) {
-        val initialGame = BingoParser.parse(this)
-
-        BingoLoosingPlayer(initialGame).getLoosingScore().toString()
-    }
-}
