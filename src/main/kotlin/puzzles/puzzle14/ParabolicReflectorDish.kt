a2cab50cf7263545ce22ee85e7446869cd5406d4
Thomas Salzinger <thomas.salzinger@tractive.com>
2023-12-14 23:02:53 +0100

    Day 14 - Part 2 Parabolic Reflector Dish

--- src/main/kotlin/puzzles/puzzle14/ParabolicReflectorDish.kt
+++ src/main/kotlin/puzzles/puzzle14/ParabolicReflectorDish.kt
@@ -15,14 +15,59 @@ object ParabolicReflectorDish {
             .sum()
     }
 
+    fun Sequence<String>.getTotalLoadOnNorthBeamAfterSpins(spinCount: Long): Int {
+        return spin(spinCount)
+            .getLoadPerRow()
+            .sum()
+    }
+
     fun Sequence<String>.tiltNorth(): List<String> {
         return toList().tiltNorth()
+    }
+
+    fun Sequence<String>.spin(spinCount: Long): List<String> {
+        val cache = mutableMapOf<List<String>, Long>()
+
+        return (1..spinCount).fold(toList()) { currentArrangement, index ->
+            val cacheIndex = cache[currentArrangement]
+            if (cacheIndex != null) {
+                val cycleTime = index - cacheIndex
+                val endArrangementIndex = cacheIndex + ((spinCount - cacheIndex) % cycleTime) + 1
+                return cache.entries.single { it.value == endArrangementIndex }.key
+            } else {
+                cache[currentArrangement] = index
+            }
+
+            currentArrangement
+                .tiltNorth()
+                .tiltWest()
+                .tiltSouth()
+                .tiltEast()
+        }
     }
 
     fun List<String>.tiltNorth(): List<String> {
         return transpose()
             .shiftRoundedRocksToLeft()
             .transpose()
+    }
+
+    fun List<String>.tiltWest(): List<String> {
+        return shiftRoundedRocksToLeft()
+    }
+
+    fun List<String>.tiltEast(): List<String> {
+        return reverseRows().shiftRoundedRocksToLeft().reverseRows()
+    }
+
+    fun List<String>.tiltSouth(): List<String> {
+        return transpose().reverseRows().shiftRoundedRocksToLeft().reverseRows().transpose()
+    }
+
+    fun List<String>.reverseRows(): List<String> {
+        return map {
+            it.reversed()
+        }
     }
 
     fun List<String>.shiftRoundedRocksToLeft(): List<String> {
--- src/test/kotlin/puzzles/puzzle14/ParabolicReflectorDishTest.kt
+++ src/test/kotlin/puzzles/puzzle14/ParabolicReflectorDishTest.kt
@@ -4,6 +4,8 @@ import assertIterable
 import assertThat
 import me.salzinger.common.streamInput
 import me.salzinger.puzzles.puzzle14.ParabolicReflectorDish.getTotalLoadOnNorthBeamAfterATiltToNorth
+import me.salzinger.puzzles.puzzle14.ParabolicReflectorDish.getTotalLoadOnNorthBeamAfterSpins
+import me.salzinger.puzzles.puzzle14.ParabolicReflectorDish.spin
 import me.salzinger.puzzles.puzzle14.ParabolicReflectorDish.tiltNorth
 import org.junit.jupiter.api.Test
 
@@ -26,6 +28,88 @@ class ParabolicReflectorDishTest {
             .getTotalLoadOnNorthBeamAfterATiltToNorth()
             .assertThat {
                 isEqualTo(136)
+            }
+    }
+
+    @Test
+    fun `example 1 - part 2`() {
+        "puzzles/puzzle14/puzzle-14-example-1.in"
+            .streamInput()
+            .getTotalLoadOnNorthBeamAfterSpins(1_000_000_000)
+            .assertThat {
+                isEqualTo(64)
+            }
+    }
+
+    @Test
+    fun `example 1 - spin once`() {
+        "puzzles/puzzle14/puzzle-14-example-1.in"
+            .streamInput()
+            .spin(1)
+            .joinToString("\n")
+            .assertThat {
+                isEqualTo(
+                    """
+                        .....#....
+                        ....#...O#
+                        ...OO##...
+                        .OO#......
+                        .....OOO#.
+                        .O#...O#.#
+                        ....O#....
+                        ......OOOO
+                        #...O###..
+                        #..OO#....
+                    """.trimIndent()
+                )
+            }
+    }
+
+    @Test
+    fun `example 1 - spin twice`() {
+        "puzzles/puzzle14/puzzle-14-example-1.in"
+            .streamInput()
+            .spin(2)
+            .joinToString("\n")
+            .assertThat {
+                isEqualTo(
+                    """
+                        .....#....
+                        ....#...O#
+                        .....##...
+                        ..O#......
+                        .....OOO#.
+                        .O#...O#.#
+                        ....O#...O
+                        .......OOO
+                        #..OO###..
+                        #.OOO#...O
+                    """.trimIndent()
+                )
+            }
+    }
+
+    @Test
+    fun `example 1 - spin three times`() {
+        "puzzles/puzzle14/puzzle-14-example-1.in"
+            .streamInput()
+            .spin(3)
+            .joinToString("\n")
+            .assertThat {
+                isEqualTo(
+                    """
+                        .....#....
+                        ....#...O#
+                        .....##...
+                        ..O#......
+                        .....OOO#.
+                        .O#...O#.#
+                        ....O#...O
+                        .......OOO
+                        #...O###.O
+                        #.OOO#...O
+                    """.trimIndent()
+                )
             }
     }
 
