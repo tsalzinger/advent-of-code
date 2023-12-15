44503e49af4cc5cf3a6db4354c4de16b96d19c45
Thomas Salzinger <thomas.salzinger@tractive.com>
2023-12-15 10:19:28 +0100

    Day 15 - Part 2 Lens Library

--- src/main/kotlin/puzzles/puzzle15/LensLibrary.kt
+++ src/main/kotlin/puzzles/puzzle15/LensLibrary.kt
@@ -17,4 +17,102 @@ object LensLibrary {
                 it.toLong()
             }
     }
+
+    class Box(
+        val boxNumber: Int,
+        staringLenses: List<Lens> = emptyList(),
+    ) {
+        private val lenses = staringLenses.toMutableList()
+
+        fun insertOrReplaceLens(lens: Lens) {
+            if (lenses.any { it.label == lens.label }) {
+                lenses.replaceAll {
+                    if (it.label == lens.label) {
+                        lens
+                    } else {
+                        it
+                    }
+                }
+            } else {
+                lenses += lens
+            }
+
+        }
+
+        fun removeLensWithLabel(lensLabel: String) {
+            lenses.removeIf {
+                it.label == lensLabel
+            }
+        }
+
+        fun getFocusPower(): Int {
+            return lenses
+                .mapIndexed { index, lens ->
+                    (boxNumber + 1) * (index + 1) * lens.focalLength
+                }
+                .sum()
+        }
+
+        override fun toString(): String {
+            return "Box $boxNumber: ${lenses.joinToString(" ") { "[${it.label} ${it.focalLength}]" }}"
+        }
+    }
+
+    sealed interface Operation {
+        val label: String
+
+        data class InsertOrReplace(
+            override val label: String,
+            val focalLength: Int,
+        ) : Operation
+
+        data class Remove(
+            override val label: String,
+        ) : Operation
+    }
+
+    fun Sequence<String>.getSumOfFocusPowerPerLens(): Long {
+        return single()
+            .split(",")
+            .map { operation ->
+                val parts = operation.trimEnd { it == '-' }.split("=")
+
+                when (parts.count()) {
+                    1 -> Operation.Remove(parts.first())
+                    2 -> Operation.InsertOrReplace(parts.first(), parts[1].toInt())
+                    else -> throw RuntimeException("Failed to get operation from $operation")
+                }
+            }
+            .fold(mutableMapOf<Int, Box>()) { boxes, operation ->
+                val boxNumber = operation.label.getHash()
+
+                boxes.compute(boxNumber) { _, boxOrNull ->
+                    val box = boxOrNull ?: Box(boxNumber)
+
+                    when (operation) {
+                        is Operation.InsertOrReplace -> box.insertOrReplaceLens(
+                            Lens(
+                                operation.label,
+                                operation.focalLength
+                            )
+                        )
+
+                        is Operation.Remove -> box.removeLensWithLabel(operation.label)
+                    }
+
+                    box
+                }
+
+                boxes
+            }
+            .values
+            .sumOf {
+                it.getFocusPower().toLong()
+            }
+    }
+
+    data class Lens(
+        val label: String,
+        val focalLength: Int,
+    )
 }
--- src/test/kotlin/puzzles/puzzle15/LensLibraryTest.kt
+++ src/test/kotlin/puzzles/puzzle15/LensLibraryTest.kt
@@ -2,7 +2,9 @@ package puzzles.puzzle15
 
 import assertThat
 import me.salzinger.common.streamInput
+import me.salzinger.puzzles.puzzle15.LensLibrary
 import me.salzinger.puzzles.puzzle15.LensLibrary.getHash
+import me.salzinger.puzzles.puzzle15.LensLibrary.getSumOfFocusPowerPerLens
 import me.salzinger.puzzles.puzzle15.LensLibrary.getSumOfHashes
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
@@ -11,7 +13,6 @@ import org.junit.jupiter.api.TestFactory
 
 class LensLibraryTest {
 
-
     @Test
     fun `example 1 - part 1`() {
         "puzzles/puzzle15/puzzle-15-example-1.in"
@@ -19,6 +20,17 @@ class LensLibraryTest {
             .getSumOfHashes()
             .assertThat {
                 isEqualTo(1320L)
+            }
+    }
+
+
+    @Test
+    fun `example 1 - part 2`() {
+        "puzzles/puzzle15/puzzle-15-example-1.in"
+            .streamInput()
+            .getSumOfFocusPowerPerLens()
+            .assertThat {
+                isEqualTo(145L)
             }
     }
 
@@ -40,6 +52,45 @@ class LensLibraryTest {
             DynamicTest.dynamicTest("Hash of $input") {
                 input
                     .getHash()
+                    .assertThat {
+                        isEqualTo(expectedHash)
+                    }
+            }
+        }
+    }
+
+    @TestFactory
+    fun `focus power per box`(): Iterable<DynamicNode> {
+        return listOf(
+            LensLibrary.Box(
+                0,
+                listOf(
+                    LensLibrary.Lens(
+                        "rn", 1,
+                    ),
+                    LensLibrary.Lens(
+                        "cm", 2,
+                    ),
+                )
+            ) to 5,
+            LensLibrary.Box(
+                3,
+                listOf(
+                    LensLibrary.Lens(
+                        "ot", 7,
+                    ),
+                    LensLibrary.Lens(
+                        "ab", 5,
+                    ),
+                    LensLibrary.Lens(
+                        "pc", 6,
+                    ),
+                )
+            ) to 140,
+        ).map { (box, expectedHash) ->
+            DynamicTest.dynamicTest("Focus power of $box") {
+                box
+                    .getFocusPower()
                     .assertThat {
                         isEqualTo(expectedHash)
                     }
