1b66c8365342327bbe95db2e4c8a338e9023a430
Thomas Salzinger <thomas.salzinger@tractive.com>
2023-12-16 21:59:45 +0100

    Day 13 - Part 2 Point of Incidence

--- src/main/kotlin/puzzles/puzzle13/PointOfIncidence.kt
+++ src/main/kotlin/puzzles/puzzle13/PointOfIncidence.kt
@@ -3,9 +3,41 @@ package me.salzinger.puzzles.puzzle13
 import me.salzinger.common.extensions.ChunkEvaluation
 import me.salzinger.common.extensions.chunkedBy
 
+
+typealias IndexedLines = Pair<Int, String>
+
 object PointOfIncidence {
 
+    fun String.countDifferences(other: String): Int {
+        return this.zip(other)
+            .count { (first, second) ->
+                first != second
+            }
+    }
+
+    fun List<String>.pairsOfLinesWithOneDifference(): List<Pair<IndexedLines, IndexedLines>> {
+        return foldIndexed(emptyList()) { index, pairs, line ->
+            val firstPair = index to line
+            pairs + drop(index + 1)
+                .mapIndexedNotNull { otherIndex, otherLine ->
+                    if (line.countDifferences(otherLine) == 1) {
+                        (index + otherIndex + 1) to otherLine
+                    } else {
+                        null
+                    }
+                }
+                .map {
+                    firstPair to it
+                }
+        }
+    }
+
     fun List<String>.getMirrorIndexOrNull(): Int? {
+        return getMirrorIndexes()
+            .singleOrNull()
+    }
+
+    fun List<String>.getMirrorIndexes(): List<Int> {
         return windowed(2)
             .mapIndexed { index, (first, second) ->
                 (index + 1) to (first to second)
@@ -13,7 +45,7 @@ object PointOfIncidence {
             .filter { (_, pair) ->
                 pair.first == pair.second
             }
-            .singleOrNull { (index, _) ->
+            .filter { (index, _) ->
                 if (index <= count() / 2) {
                     slice(index..<2 * index) == slice(0..<index).reversed()
                 } else {
@@ -21,15 +53,13 @@ object PointOfIncidence {
                     slice((index - sliceLength)..<index) == slice(index..<count()).reversed()
                 }
             }
-            ?.first
+            .map {
+                it.first
+            }
     }
 
     fun List<String>.getMirrorIndex(): Int {
         return getMirrorIndexOrNull() ?: throw RuntimeException("Failed to find mirror index in $this")
-    }
-
-    fun List<String>.getMirrorSummaryOrNull(multiplier: Int): Int? {
-        return getMirrorIndexOrNull()?.times(multiplier)
     }
 
     fun List<String>.transpose(): List<String> {
@@ -40,7 +70,7 @@ object PointOfIncidence {
         }
     }
 
-    fun Sequence<String>.getSummaryOfNotes(): Int {
+    fun Sequence<String>.separatePatterns(): List<List<String>> {
         return toList()
             .chunkedBy {
                 when {
@@ -48,8 +78,65 @@ object PointOfIncidence {
                     else -> ChunkEvaluation.APPEND_TO_CHUNK
                 }
             }
+    }
+
+    fun Sequence<String>.getSummaryOfNotes(): Int {
+        return separatePatterns()
             .sumOf {
                 it.getMirrorIndexOrNull()?.times(100) ?: it.transpose().getMirrorIndex()
             }
+    }
+
+    fun Sequence<String>.getSummaryOfNotesAfterCorrectingTheSmudge(): Int {
+        return separatePatterns()
+            .map { pattern ->
+                val rowIndexWithoutCorrection = pattern.getMirrorIndexOrNull()
+                val rowWise = pattern
+                    .pairsOfLinesWithOneDifference()
+                    .filter { (it.second.first - it.first.first) % 2 == 1 }
+                    .map { (line1, line2) ->
+                        List(pattern.count()) { index ->
+                            if (index == line2.first) {
+                                line1.second
+                            } else {
+                                pattern[index]
+                            }
+                        }
+                    }
+                    .flatMap {
+                        it.getMirrorIndexes()
+                    }
+                    .toSet()
+                    .filter {
+                        it != rowIndexWithoutCorrection
+                    }
+
+                val transposed = pattern.transpose()
+                val columnIndexWithoutCorrection = transposed.getMirrorIndexOrNull()
+                val columnWise = transposed
+                    .pairsOfLinesWithOneDifference()
+                    .filter { (it.second.first - it.first.first) % 2 == 1 }
+                    .map { (line1, line2) ->
+                        List(transposed.count()) { index ->
+                            if (index == line2.first) {
+                                line1.second
+                            } else {
+                                transposed[index]
+                            }
+                        }
+                    }
+                    .flatMap {
+                        it.getMirrorIndexes()
+                    }
+                    .toSet()
+                    .filter {
+                        it != columnIndexWithoutCorrection
+                    }
+
+                require(rowWise.count() + columnWise.count() == 1)
+
+                rowWise.singleOrNull()?.times(100) ?: columnWise.single()
+            }
+            .sum()
     }
 }
--- src/test/kotlin/puzzles/puzzle13/PointOfIncidenceTest.kt
+++ src/test/kotlin/puzzles/puzzle13/PointOfIncidenceTest.kt
@@ -3,6 +3,7 @@ package puzzles.puzzle13
 import assertThat
 import me.salzinger.common.streamInput
 import me.salzinger.puzzles.puzzle13.PointOfIncidence.getSummaryOfNotes
+import me.salzinger.puzzles.puzzle13.PointOfIncidence.getSummaryOfNotesAfterCorrectingTheSmudge
 import me.salzinger.puzzles.puzzle13.PointOfIncidence.transpose
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
@@ -37,6 +38,36 @@ class PointOfIncidenceTest {
             .getSummaryOfNotes()
             .assertThat {
                 isEqualTo(400)
+            }
+    }
+
+    @Test
+    fun `example 1 - part 2`() {
+        "puzzles/puzzle13/puzzle-13-example-1.in"
+            .streamInput()
+            .getSummaryOfNotesAfterCorrectingTheSmudge()
+            .assertThat {
+                isEqualTo(400)
+            }
+    }
+
+    @Test
+    fun `example 2 - part 2`() {
+        "puzzles/puzzle13/puzzle-13-example-2.in"
+            .streamInput()
+            .getSummaryOfNotesAfterCorrectingTheSmudge()
+            .assertThat {
+                isEqualTo(300)
+            }
+    }
+
+    @Test
+    fun `example 3 - part 2`() {
+        "puzzles/puzzle13/puzzle-13-example-3.in"
+            .streamInput()
+            .getSummaryOfNotesAfterCorrectingTheSmudge()
+            .assertThat {
+                isEqualTo(100)
             }
     }
 
