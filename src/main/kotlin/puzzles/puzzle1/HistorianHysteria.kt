6d0bfb8b21b63877093218d6810b91fb305c17c7
Thomas Salzinger <thomas@salzinger.me>
2024-12-01 21:26:03 +0100

    Day 1 - Part 2 Historian Hysteria

--- src/main/kotlin/puzzles/puzzle1/HistorianHysteria.kt
+++ src/main/kotlin/puzzles/puzzle1/HistorianHysteria.kt
@@ -5,6 +5,7 @@ import me.salzinger.common.extensions.toPairs
 import kotlin.math.absoluteValue
 
 object HistorianHysteria {
+
     fun Sequence<String>.sumOfDifferences(): Int {
         return map {
             it.toIntList("   ").toPairs().single()
@@ -17,5 +18,22 @@ object HistorianHysteria {
             }.sumOf { (first, second) ->
                 (second - first).absoluteValue
             }
+    }
+
+    fun Sequence<String>.getSimilarityScore(): Int {
+        return map {
+            it.toIntList("   ").toPairs().single()
+        }
+            .fold(listOf<Int>() to listOf<Int>()) { (firstList, secondList), (first, second) ->
+                (firstList + first) to (secondList + second)
+            }
+            .let { (fistList, secondList) ->
+                val occurrences = secondList.groupingBy { it }.eachCount()
+
+                fistList.groupingBy { it }
+                    .eachCount()
+                    .map { it.key * (occurrences[it.key] ?: 0) * it.value }
+            }
+            .sum()
     }
 }
