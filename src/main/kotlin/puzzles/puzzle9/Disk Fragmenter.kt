722692b68f92757b75da324a3468b05e22069d72
Thomas Salzinger <thomas@salzinger.me>
2024-12-09 22:58:28 +0100

    Day 9 - Part 2 Disk Fragmenter
    Example is working as expected but puzzle seems to be wrong (answer is too high)

--- src/main/kotlin/puzzles/puzzle9/Disk Fragmenter.kt
+++ src/main/kotlin/puzzles/puzzle9/Disk Fragmenter.kt
@@ -9,6 +9,44 @@ object `Disk Fragmenter` {
             .checksum()
     }
 
+    fun Sequence<String>.compactedUnfragmentedFilesystemChecksum(): Long {
+        return single()
+            .map { "$it".toInt() }
+            .toDiskBlockGroups()
+            .compactUnfragmented()
+            .flatMap { diskBlockGroup ->
+                List(diskBlockGroup.length) {
+                    diskBlockGroup.diskBlock
+                }
+            }
+            .checksum()
+    }
+
+    private fun List<DiskBlockGroup>.compactUnfragmented(): List<DiskBlockGroup> {
+        val alreadyEvaluatedBlocks = mutableSetOf<DiskBlockGroup>()
+        var fileBlocKToMove = lastOrNull { it.diskBlock is DiskBlock.File }
+        val compacted = toMutableList()
+
+        while (fileBlocKToMove != null) {
+            alreadyEvaluatedBlocks += fileBlocKToMove
+            val canFit = compacted.firstOrNull { it.canFit(fileBlocKToMove) }
+
+            if (canFit != null) {
+                val indexToMove = compacted.indexOf(fileBlocKToMove)
+                compacted.removeAt(indexToMove)
+                compacted.add(indexToMove, DiskBlockGroup(DiskBlock.FreeSpace, fileBlocKToMove.length))
+
+                val indexToReplace = compacted.indexOf(canFit)
+                compacted.removeAt(indexToReplace)
+                compacted.addAll(indexToReplace, canFit.insert(fileBlocKToMove))
+            }
+
+            fileBlocKToMove = lastOrNull { it.diskBlock is DiskBlock.File && it !in alreadyEvaluatedBlocks }
+        }
+
+        return compacted
+    }
+
     fun List<Int>.toDiskBlocks(): List<DiskBlock> {
         val diskMap = this
         return buildList {
@@ -25,6 +63,29 @@ object `Disk Fragmenter` {
                         }
                     }
                 }
+        }
+    }
+
+    fun List<Int>.toDiskBlockGroups(): List<DiskBlockGroup> {
+        val diskMap = this
+        return buildList {
+            diskMap.forEachIndexed { index, blockLength ->
+                if (index % 2 == 0) {
+                    add(
+                        DiskBlockGroup(
+                            diskBlock = DiskBlock.File(id = index / 2),
+                            length = blockLength,
+                        )
+                    )
+                } else {
+                    add(
+                        DiskBlockGroup(
+                            diskBlock = DiskBlock.FreeSpace,
+                            length = blockLength,
+                        )
+                    )
+                }
+            }
         }
     }
 
@@ -67,5 +128,36 @@ object `Disk Fragmenter` {
         object FreeSpace : DiskBlock
 
         data class File(val id: Int) : DiskBlock
+    }
+
+    data class DiskBlockGroup(
+        val diskBlock: DiskBlock,
+        val length: Int,
+    ) {
+        fun canFit(diskBlockGroup: DiskBlockGroup): Boolean {
+            check(diskBlockGroup.diskBlock is DiskBlock.File) {
+                "Only Files can fit into free blocks"
+            }
+
+            return when (diskBlock) {
+                DiskBlock.FreeSpace -> length >= diskBlockGroup.length
+                is DiskBlock.File -> false
+            }
+        }
+
+        fun insert(diskBlockGroup: DiskBlockGroup): List<DiskBlockGroup> {
+            check(canFit(diskBlockGroup)) {
+                "does not fit"
+            }
+
+            return if (length == diskBlockGroup.length) {
+                listOf(diskBlockGroup)
+            } else {
+                listOf(
+                    diskBlockGroup,
+                    copy(length = length - diskBlockGroup.length)
+                )
+            }
+        }
     }
 }
