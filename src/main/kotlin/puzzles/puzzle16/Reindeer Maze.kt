537ecb387167617d0ed8feb62e4b4f32380173f0
Thomas Salzinger <thomas@salzinger.me>
2024-12-16 21:01:52 +0100

    Day 16 - Part 2 Reindeer Maze

--- src/main/kotlin/puzzles/puzzle16/Reindeer Maze.kt
+++ src/main/kotlin/puzzles/puzzle16/Reindeer Maze.kt
@@ -58,4 +58,69 @@ object `Reindeer Maze` {
 
         return lowestScores.getValue(endCell.coordinate)
     }
+
+    fun Sequence<String>.countTilesOnBestPaths(): Int {
+        val maze = toGrid2D(neighborProvider = Grid2D.Coordinate.NeighborModes.CROSS)
+
+        val startCell = maze.single { it.value == 'S' }
+        val endCell = maze.single { it.value == 'E' }
+
+        val lowestScores = mutableMapOf(
+            startCell.coordinate to 0L
+        )
+
+        var currentPositions = listOf(
+            Triple(listOf(startCell.coordinate), Direction.RIGHT, 0L),
+        )
+
+        val bestPathCandidates = mutableSetOf<Pair<List<Grid2D.Coordinate>, Long>>()
+
+        while (currentPositions.isNotEmpty()) {
+            currentPositions = currentPositions
+                .flatMap { (pathSoFar, currentDirection, currentScore) ->
+                    POSSIBLE_MOVEMENT_DIRECTIONS
+                        .mapNotNull { direction ->
+                            pathSoFar.last()(direction)
+                                .takeIf { maze[it].value != '#' && it !in pathSoFar }
+                                ?.let { direction to it }
+                        }
+                        .map { (direction, position) ->
+                            Triple(
+                                pathSoFar + position,
+                                direction,
+                                when {
+                                    currentDirection == direction -> currentScore + 1
+                                    currentDirection.isOppositeOf(direction) -> currentScore + 2001
+                                    else -> currentScore + 1001
+                                }
+                            )
+                        }
+                        .filter { (path, _, score) ->
+                            (path.last() !in lowestScores) ||
+                                    ((lowestScores.getValue(path.last()) + 1000) >= score)
+                        }
+                        .onEach { (path, _, score) ->
+                            if (path.last() == endCell.coordinate) {
+                                bestPathCandidates += path to score
+                            }
+                            lowestScores[path.last()] = score
+                        }
+                }
+        }
+
+        val lowestScore = lowestScores.getValue(endCell.coordinate)
+
+
+        val bestPaths = bestPathCandidates
+            .filter { (_, score) ->
+                score == lowestScore
+            }
+            .map { it.first.toSet() }
+
+        val tilesOnBestPaths = bestPaths
+            .flatMap { it }
+            .toSet()
+
+        return tilesOnBestPaths.count()
+    }
 }
