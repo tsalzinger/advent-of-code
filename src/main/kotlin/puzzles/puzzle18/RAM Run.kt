bf9667abb812b70e0f57c37df26b6618da78e9ca
Thomas Salzinger <thomas@salzinger.me>
2024-12-18 16:56:55 +0100

    Day 18 - Part 2 RAM Run

--- src/main/kotlin/puzzles/puzzle18/RAM Run.kt
+++ src/main/kotlin/puzzles/puzzle18/RAM Run.kt
@@ -39,6 +39,12 @@ object `RAM Run` {
             neighborProvider = Coordinate.NeighborModes.CROSS,
         )
 
+        return findMinStepsOrNull(memoryMap)!!
+    }
+
+    private fun findMinStepsOrNull(
+        memoryMap: LazyGrid2D<Tile>,
+    ): Int? {
         var steps = 0
         var currentCoordinates = setOf(START_COORDINATE)
         val alreadyVisited = currentCoordinates.toMutableSet()
@@ -46,7 +52,7 @@ object `RAM Run` {
             START_COORDINATE to steps,
         )
 
-        while (END_COORDINATE !in currentCoordinates) {
+        while (END_COORDINATE !in currentCoordinates && currentCoordinates.isNotEmpty()) {
             steps++
             currentCoordinates = currentCoordinates
                 .flatMap {
@@ -70,17 +76,52 @@ object `RAM Run` {
                 }
         }
 
-        memoryMap
-            .toConsoleString {
-                when (it.coordinate) {
-                    in unsafeCoordinates -> "\u001b[31m[ # ]\u001b[0m"
-                    in currentCoordinates -> "[ X ]"
-                    in alreadyVisited -> "[${coordinateToSteps.getValue(it.coordinate).toString().padStart(3)}]"
-                    else -> "\u001b[34m[ . ]\u001b[0m"
-                }
-            }
-            .also(::println)
 
-        return steps
+        return if (END_COORDINATE in currentCoordinates) {
+            steps
+        } else {
+            memoryMap
+                .toConsoleString {
+                    when {
+                        it.value is Tile.Unsafe -> "\u001b[31m[ # ]\u001b[0m"
+                        it.coordinate in currentCoordinates -> "[ X ]"
+                        it.coordinate in alreadyVisited -> "[${coordinateToSteps.getValue(it.coordinate).toString().padStart(3)}]"
+                        else -> "\u001b[34m[ . ]\u001b[0m"
+                    }
+                }
+                .also(::println)
+            null
+        }
+    }
+
+    fun Sequence<String>.getCoordinateOfFirstByteBlockingPath(): String {
+        val bytes = map {
+            it
+                .toPoint2D()
+                .toCoordinate()
+        }.toList()
+
+        val unsafeBytes = bytes.take(1024).toMutableSet()
+
+        val memoryMap = LazyGrid2D(
+            valuesProvider = {
+                when (it) {
+                    in unsafeBytes -> Tile.Unsafe
+                    else -> Tile.Safe
+                }
+            },
+            boundaryProvider = { BOUNDARY },
+            neighborProvider = Coordinate.NeighborModes.CROSS,
+        )
+
+
+        return bytes.takeLast(bytes.count() - 1024)
+            .first {
+                unsafeBytes += it
+                findMinStepsOrNull(memoryMap) == null
+            }
+            .run {
+                "$column,$row"
+            }
     }
 }
