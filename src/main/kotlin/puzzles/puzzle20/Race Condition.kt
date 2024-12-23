881ecb634050c8b700be2fe8e0adf4aaf566e8e8
Thomas Salzinger <thomas@salzinger.me>
2024-12-23 23:48:28 +0100

    Day 20 - Part 2 Race Condition
    Does not yet work at all, but even if it did it would be a terribly inefficient way of doing so

--- src/main/kotlin/puzzles/puzzle20/Race Condition.kt
+++ src/main/kotlin/puzzles/puzzle20/Race Condition.kt
@@ -47,7 +47,7 @@ object `Race Condition` {
             it to map.getNeighborsOf(it)
                 .filter { it.value in setOf(Tile.START, Tile.TRACK, Tile.END) }
         }
-            .filter { (_, tracks ) ->
+            .filter { (_, tracks) ->
                 tracks.count() >= 2
             }
             .flatMap { (_, tracks) ->
@@ -58,5 +58,77 @@ object `Race Condition` {
                     }
             }
             .count { it >= 100 }
+    }
+
+    fun Sequence<String>.countAvailableCheatsSavingAtLeast100PicosecondsWithUpdatedRules(): Int {
+        val map = toGrid2D(neighborProvider = Grid2D.Coordinate.NeighborModes.CROSS)
+
+        val startCoordinate = map.single { it.value == Tile.START }.coordinate
+        val endCoordinate = map.single { it.value == Tile.END }.coordinate
+
+        var steps = 0
+        var currentCoordinate = startCoordinate
+        val alreadyVisited = mutableSetOf(currentCoordinate)
+        val coordinateToSteps = mutableMapOf<Coordinate, Int>(
+            startCoordinate to steps,
+        )
+
+        while (endCoordinate != currentCoordinate) {
+            steps++
+            currentCoordinate = map.getNeighborsOf(currentCoordinate)
+                .single {
+                    it.value in setOf(Tile.TRACK, Tile.END) &&
+                            it.coordinate !in alreadyVisited
+                }
+                .coordinate
+                .also(alreadyVisited::add)
+                .also {
+                    coordinateToSteps[it] = steps
+                }
+        }
+
+        return map.filter {
+            it.value in setOf(Tile.START, Tile.TRACK, Tile.END)
+        }.permutate()
+            .filter { (first, second) -> first.coordinate.getManhattenDistanceTo(second.coordinate) <= MAX_CHEAT_LENGTH }
+            .mapNotNull {
+                map.getCheatSavingsOrNull(it.first.coordinate, it.second.coordinate, coordinateToSteps)
+            }
+            .count { it >= 100 }
+    }
+
+    val MAX_CHEAT_LENGTH = 20
+
+    fun Grid2D<Char>.getCheatSavingsOrNull(
+        startCoordinate: Coordinate,
+        endCoordinate: Coordinate,
+        coordinateToSteps: Map<Coordinate, Int>,
+    ): Int? {
+        var steps = 0
+        var currentCoordinates = setOf(startCoordinate)
+        val alreadyVisited = currentCoordinates.toMutableSet()
+
+        while (endCoordinate !in currentCoordinates && steps < MAX_CHEAT_LENGTH) {
+            steps++
+            currentCoordinates = currentCoordinates
+                .flatMap {
+                    getNeighborsOf(it)
+                }
+                .filter {
+                    it.value == Tile.WALL || it.coordinate == endCoordinate
+                }
+                .map { it.coordinate }
+                .toSet()
+                .also(alreadyVisited::addAll)
+        }
+
+        return if (endCoordinate in currentCoordinates) {
+            abs(coordinateToSteps.getValue(startCoordinate) - coordinateToSteps.getValue(endCoordinate)) - steps - 1
+                .also {
+                    println("Found cheat saving $it")
+                }
+        } else {
+            null
+        }
     }
 }
